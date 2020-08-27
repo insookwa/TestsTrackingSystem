f805e1f3b36fba2795398244777ac62e5568455d
Nsookwa Ivan <42169195+insookwa@users.noreply.github.com>
2020-08-27 13:15:26 +0200

    additional files

---
+++ src/main/java/com/raffsoft/ttsApp/TestsTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.raffsoft.ttsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsTrackingSystemApplication.class, args);
+	}
+
+}
